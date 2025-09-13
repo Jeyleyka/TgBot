c30a8ffa6638ad38fe0306747769c81f132d4644
zeles <rojomodmini3@gmail.com>
2025-09-13 22:15:31 +0500

    added 3 test buttons

--- Networking/core.cpp
+++ Networking/core.cpp
@@ -3,8 +3,8 @@
 std::unordered_map<std::string, std::any> WinBot::setUpCommands() noexcept {
 	auto commands = std::unordered_map<std::string, std::any>{};
 
-	commands["info"] = std::bind(&WinBot::onInfo, this, std::placeholders::_1);
 	commands["start"] = std::bind(&WinBot::onCommandStart, this, std::placeholders::_1);
+	commands["info"] = std::bind(&WinBot::onInfo, this, std::placeholders::_1);
 	commands["inline"] = std::bind(&WinBot::onCommandKeybord, this, std::placeholders::_1);
 
 	return commands;
@@ -13,9 +13,9 @@ std::unordered_map<std::string, std::any> WinBot::setUpCommands() noexcept {
 std::unordered_map<std::string, std::any> WinBot::setUpCallbacks() noexcept {
 	auto callbacks = std::unordered_map<std::string, std::any>{};
 
-	callbacks["info"] = std::bind(&WinBot::onInfo, this, std::placeholders::_1);
-	callbacks["start"] = std::bind(&WinBot::onCommandStart, this, std::placeholders::_1);
-	callbacks["inline"] = std::bind(&WinBot::onCommandKeybord, this, std::placeholders::_1);
+	callbacks["Current time"] = std::bind(&WinBot::onCurrentTime, this, std::placeholders::_1);
+	callbacks["GitHub"] = std::bind(&WinBot::onGithubMenu, this, std::placeholders::_1);
+	callbacks["C++"] = std::bind(&WinBot::onCommandKeybord, this, std::placeholders::_1);
 
 	return callbacks;
 }
@@ -23,26 +23,57 @@ std::unordered_map<std::string, std::any> WinBot::setUpCallbacks() noexcept {
 void WinBot::onInfo(TgBot::Message::Ptr message)
 {
 	bot.getApi().sendMessage(message->chat->id,
-		"Available commands:\n"
-		"/start - greetings\n"
-		"/time - current time");
+		"/start  Start\n"
+		"/info Ways\n"
+		"/inline NaVi\n"
+	);
 }
 
 void WinBot::onCommandStart(TgBot::Message::Ptr message)
 {
 	bot.getApi().sendMessage(
 			message->chat->id,
-			"Hello user!"
+			"Hello user, Type /info for help."
 	);
 }
 
 void WinBot::onCommandKeybord(TgBot::Message::Ptr message)
 {
+	bot.getApi().sendMessage(
+		message->chat->id,
+		"Choosing action",
+		false,
+		0,
+		keyboard
+	);
+}
+
+void WinBot::onGithubMenu(TgBot::CallbackQuery::Ptr query)
+{
+	bot.getApi().sendMessage(
+		query->message->chat->id,
+		std::string("Repository:\n https://github.com/Jeyleyka/TgBot")
+	);
+}
+
+void WinBot::onCurrentTime(TgBot::CallbackQuery::Ptr query)
+{
+	auto now = boost::asio::chrono::system_clock::now();
+		std::time_t now_time = boost::asio::chrono::system_clock::to_time_t(now);
+
+		std::tm local_tm{};
+		localtime_s(&local_tm, &now_time);
+
+		std::stringstream ss;
+		ss << std::put_time(&local_tm, "%Y-%m-%d %H:%M:%S");
+
+	bot.getApi().sendMessage(query->message->chat->id, std::string("Current time: ") + ss.str());
 }
 
 WinBot::WinBot(const std::string& token)
-	: token(token), bot(token)
+	: token(token), bot(token), keyboard(new TgBot::InlineKeyboardMarkup)
 {
+	// Processing all commands
 	for (auto& c : setUpCommands()) {
 		auto func = std::any_cast<std::_Binder<
 			std::_Unforced,
@@ -56,6 +87,7 @@ WinBot::WinBot(const std::string& token)
 		});
 	}
 
+	// Processing all buttons
 	bot.getEvents().onCallbackQuery([this](TgBot::CallbackQuery::Ptr query) {
 		for (auto& c : setUpCallbacks()) {
 			if (c.first == query->data) {
@@ -70,31 +102,30 @@ WinBot::WinBot(const std::string& token)
 				return;
 			}
 		}
+
+		bot.getApi().sendMessage(query->message->chat->id, "Unknown control!!!");
 	});
 
+	
+	// Creating btns
+	TgBot::InlineKeyboardButton::Ptr btn1(new TgBot::InlineKeyboardButton);
+	btn1->text = "Current time";
+	btn1->callbackData = "Current time";
 
-	bot.getEvents().onCommand("time", [this](TgBot::Message::Ptr message) {
-		auto now = boost::asio::chrono::system_clock::now();
-		std::time_t now_time = boost::asio::chrono::system_clock::to_time_t(now);
+	TgBot::InlineKeyboardButton::Ptr btn2(new TgBot::InlineKeyboardButton);
+	btn2->text = "GitHub";
+	btn2->callbackData = "GitHub";
 
-		std::tm local_tm{};
-		localtime_s(&local_tm, &now_time);
+	TgBot::InlineKeyboardButton::Ptr btn3(new TgBot::InlineKeyboardButton);
+	btn3->text = "C++";
+	btn3->callbackData = "C++";
 
-		std::stringstream ss;
-		ss << std::put_time(&local_tm, "%Y-%m-%d %H:%M:%S");
+	row.push_back(btn1);
+	row.push_back(btn2);
+	row.push_back(btn3);
 
-		bot.getApi().sendMessage(
-			message->chat->id,
-			"Current time: " + ss.str()
-		);
-	});
-
-	bot.getEvents().onCommand("echo", [this](TgBot::Message::Ptr message) {
-		bot.getApi().sendMessage(
-			message->chat->id,
-			message->text.erase(0, 5)
-		);
-	});
+	// Add buttons to the screen
+	keyboard->inlineKeyboard.push_back(row);
 }
 
 void WinBot::run() {
--- Networking/core.h
+++ Networking/core.h
@@ -7,6 +7,7 @@
 #include <functional>
 #include <any>
 #include <unordered_map>
+#include <vector>
 
 #include <tgbot/tgbot.h>
 
@@ -17,13 +18,21 @@ class WinBot
 private:
 	std::string token;
 	TgBot::Bot bot;
+	TgBot::InlineKeyboardMarkup::Ptr keyboard;
+	std::vector<TgBot::InlineKeyboardButton::Ptr> row;
+	
 
 	std::unordered_map<std::string, std::any> setUpCommands() noexcept;
 	std::unordered_map<std::string, std::any> setUpCallbacks() noexcept;
 
+
 	void onInfo(TgBot::Message::Ptr message);
 	void onCommandStart(TgBot::Message::Ptr message);
 	void onCommandKeybord(TgBot::Message::Ptr message);
+
+	void onGithubMenu(TgBot::CallbackQuery::Ptr query);
+	void onCurrentTime(TgBot::CallbackQuery::Ptr query);
+	//void onGithubMenu(TgBot::CallbackQuery::Ptr query);
 
 public:
 	WinBot(const std::string& token);
